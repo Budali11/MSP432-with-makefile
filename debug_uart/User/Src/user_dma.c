f16ac941a47a61b24075615a61e8eaada342b0ab
Budali11 <m13058266662@163.com>
2022-10-23 23:49:37 +0800

    normal update

---
+++ debug_uart/User/Src/user_dma.c
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2022 Budali11
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "msp.h"
+
+static uint32_t data_control_structure[][4] __attribute__((aligned(16))) = 
+{
+    {0x12345678, 0x12345678, 0, 0},
+};
+
+
---
+++ debug_uart/main.c
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2022 Budali11
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "user_core.h"
+#include "iic_bus.h"
+#include "flash.h"
+#include "pcm.h"
+#include "gpio.h"
+#include "cs.h"
+
+void Clock_Configuration(void);
+void Clock_Information(void);
+
+
+int main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+    GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
+	Clock_Configuration();
+
+    while (1)
+    {
+        /* code */
+    }
+    
+
+    return 0;
+}
+
+void Clock_Configuration(void)
+{
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_PJ,
+        GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+    CS_setExternalClockSourceFrequency(32000, 48000000);
+
+    PCM_setCoreVoltageLevel(PCM_VCORE1);
+    FlashCtl_setWaitState(FLASH_BANK0, 2);
+    FlashCtl_setWaitState(FLASH_BANK1, 2);
+    CS_startHFXT(false);
+    CS_initClockSignal(CS_ACLK, CS_LFXTCLK_SELECT, CS_CLOCK_DIVIDER_1); //ACLK == 32.768kHz
+    CS_initClockSignal(CS_MCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_1); //MCLK == 48MHz
+    CS_initClockSignal(CS_HSMCLK, CS_MODOSC_SELECT, CS_CLOCK_DIVIDER_1); //HSMCLK == 24MHz
+    CS_initClockSignal(CS_SMCLK, CS_MODOSC_SELECT, CS_CLOCK_DIVIDER_2); //SMCLK == 12MHz
+    CS_initClockSignal(CS_BCLK, CS_LFXTCLK_SELECT, CS_CLOCK_DIVIDER_1); //BCLK == 32.768MHz
+
+}
+
+void Clock_Information(void)
+{
+    uint32_t ACLK, MCLK, HSMCLK, SMCLK, BCLK;
+
+    ACLK = CS_getACLK();
+    MCLK = CS_getMCLK();
+    HSMCLK = CS_getHSMCLK();
+    SMCLK = CS_getSMCLK();
+    BCLK = CS_getBCLK();
+
+    (void)ACLK;
+    (void)MCLK;
+    (void)HSMCLK;
+    (void)SMCLK;
+    (void)BCLK;
+}
