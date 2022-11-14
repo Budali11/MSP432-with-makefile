faaea095781236d6a14c30555dd3f6ee70fa7e9f
budali11 <m13058266662@163.com>
2022-11-14 23:17:42 +0800

    move clk init to preinit

--- debug_uart/main.c
+++ debug_uart/main.c
@@ -22,8 +22,6 @@
 #include "gpio.h"
 #include "cs.h"
 
-void Clock_Configuration(void);
-void Clock_Information(void);
 void Delay(uint32_t us)
 {
     for(uint32_t i = 0; i < 48*us; i++);
@@ -35,9 +33,12 @@ int main(void)
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 	GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
     GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
-	Clock_Configuration();
-    Delay(20);
+    
+    /* initialize peripheral */
     Peripheral_PreInit();
+
+    /* print clock frequency */
+    Clock_Information();
 
     Printf("Yuki.\r\n");
     D_Printf(" DMA_Yuki.\r\n");
@@ -54,25 +55,7 @@ int main(void)
     return 0;
 }
 
-void Clock_Configuration(void)
-{
-    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_PJ,
-        GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
-    CS_setExternalClockSourceFrequency(32000, 48000000);
-
-    PCM_setCoreVoltageLevel(PCM_VCORE1);
-    FlashCtl_setWaitState(FLASH_BANK0, 2);
-    FlashCtl_setWaitState(FLASH_BANK1, 2);
-    CS_startHFXT(false);
-    CS_initClockSignal(CS_ACLK, CS_LFXTCLK_SELECT, CS_CLOCK_DIVIDER_1); //ACLK == 32.768kHz
-    CS_initClockSignal(CS_MCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_1); //MCLK == 48MHz
-    CS_initClockSignal(CS_HSMCLK, CS_MODOSC_SELECT, CS_CLOCK_DIVIDER_1); //HSMCLK == 24MHz
-    CS_initClockSignal(CS_SMCLK, CS_MODOSC_SELECT, CS_CLOCK_DIVIDER_2); //SMCLK == 12MHz
-    CS_initClockSignal(CS_BCLK, CS_LFXTCLK_SELECT, CS_CLOCK_DIVIDER_1); //BCLK == 32.768MHz
-
-}
-
-void Clock_Information(void)
+static void Clock_Information(void)
 {
     uint32_t ACLK, MCLK, HSMCLK, SMCLK, BCLK;
 
@@ -88,3 +71,24 @@ void Clock_Information(void)
     (void)SMCLK;
     (void)BCLK;
 }
+
+static int Clock_Configuration(void)
+{
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_PJ,
+        GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+    CS_setExternalClockSourceFrequency(32000, 48000000);
+
+    PCM_setCoreVoltageLevel(PCM_VCORE1);
+    FlashCtl_setWaitState(FLASH_BANK0, 2);
+    FlashCtl_setWaitState(FLASH_BANK1, 2);
+    CS_startHFXT(false);
+    CS_initClockSignal(CS_ACLK, CS_LFXTCLK_SELECT, CS_CLOCK_DIVIDER_1); //ACLK == 32.768kHz
+    CS_initClockSignal(CS_MCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_1); //MCLK == 48MHz
+    CS_initClockSignal(CS_HSMCLK, CS_MODOSC_SELECT, CS_CLOCK_DIVIDER_1); //HSMCLK == 24MHz
+    CS_initClockSignal(CS_SMCLK, CS_MODOSC_SELECT, CS_CLOCK_DIVIDER_2); //SMCLK == 12MHz
+    CS_initClockSignal(CS_BCLK, CS_LFXTCLK_SELECT, CS_CLOCK_DIVIDER_1); //BCLK == 32.768MHz
+
+
+    return 0;
+}
+preinit(Clock_Configuration);
